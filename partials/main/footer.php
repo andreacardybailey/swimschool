ff6e7267c08878981b43502be0e4853f68dc7478
Andrea Cardy <acardy@customink.com>
2014-11-09 23:40:34 -0500

    Reorganizing code, added slidebar, working...

--- partials/main/footer.php
+++ partials/main/footer.php
@@ -1,7 +1,13 @@
-  <footer>
-    <div class="footer-copyright">
-      &copy; 2014 Saint Andrew's Swim School. All Rights Reserved.
-    </div>
-  </footer>
+    <footer>
+      <div class="footer-copyright">
+        <div class="ss-wrapper">
+          &copy; 2014 Saint Andrew's Swim School. All Rights Reserved.
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="/bower_components/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="/assets/javascripts/vendor/slidebars.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="/assets/javascripts/swimschool.js" type="text/javascript" charset="utf-8"></script>
   </body>
 </html>
--- partials/main/header.php
+++ partials/main/header.php
@@ -1,34 +1,59 @@
-<header>
-  <section class="mobile-menu mobile">
-    <a href="javascript:void(0)" class="menu">Menu</a>
-    <a href="tel:+18443527946">Contact Us</a>
-  </section>
-  <div class="logo">
-    Florida Swim School
-  </div>
-  <aside class="general-info">
-    Toll Free: 844-FLA-SWIM &nbsp;Local: 561-288-3817
-    <ul>
-      <li><a href="faqs.php">FAQs</a></li>
-      <li><a href="locations.php">LOCATIONS</a></li>
-      <li><a href="contact.php">CONTACT US</a></li>
-    </ul>
-  </aside>
-  <nav>
-    <a href="register.php" class="register-now">REGISTER NOW</a>
-    <ul>
-      <li><a href="/">HOME</a></li>
-      <li><a href="about.php">ABOUT US</a></li>
-      <li><a href="lessons.php">LESSONS</a></li>
-      <li><a href="coaches.php">COACHES</a></li>
-      <li><a href="leaders.php">LEADERS</a></li>
-      <li><a href="gallery.php">GALLERY</a></li>
-      <li><a href="gallery.php">LATEST NEWS</a></li>
-    </ul>
-    <ul class="mobile">
-      <li><a href="faqs.php">FAQs</a></li>
-      <li><a href="locations.php">LOCATIONS</a></li>
-      <li><a href="contact.php">CONTACT US</a></li>
-    </ul>
-  </nav>
-</header>
+<div class="sb-slidebar sb-left">
+  <a href="register.php" class="register-now">REGISTER NOW</a>
+  <ul>
+    <li><a href="/">HOME</a></li>
+    <li><a href="about.php">ABOUT US</a></li>
+    <li><a href="lessons.php">LESSONS</a></li>
+    <li><a href="coaches.php">COACHES</a></li>
+    <li><a href="leaders.php">LEADERS</a></li>
+    <li><a href="gallery.php">GALLERY</a></li>
+    <li><a href="gallery.php">LATEST NEWS</a></li>
+  </ul>
+  <ul class="mobile">
+    <li><a href="faqs.php">FAQs</a></li>
+    <li><a href="locations.php">LOCATIONS</a></li>
+    <li><a href="contact.php">CONTACT US</a></li>
+  </ul>
+</div>
+<div class="sb-site-container">
+  <header>
+      <section class="mobile-menu mobile">
+        <a href="javascript:void(0);" class="sb-open-left"><i class="icon-menu"></i>Menu</a>
+        <a href="tel:+18443527946"><i class="icon-phone"></i>Contact Us</a>
+      </section>
+      <div class="logo">
+        Florida Swim School
+      </div>
+      <div class="ss-wrapper">
+      <aside class="general-info">
+        Toll Free: 844-FLA-SWIM &nbsp;Local: 561-288-3817
+        <ul>
+          <li><a href="faqs.php">FAQs</a></li>
+          <li><a href="locations.php">LOCATIONS</a></li>
+          <li><a href="contact.php">CONTACT US</a></li>
+        </ul>
+      </aside>
+    </div>
+    <nav>
+      <!-- <div class="ss-wrapper">
+        <div style="color:white">
+          <a href="register.php" class="register-now">REGISTER NOW</a>
+          <ul>
+            <li><a href="/">HOME</a></li>
+            <li><a href="about.php">ABOUT US</a></li>
+            <li><a href="lessons.php">LESSONS</a></li>
+            <li><a href="coaches.php">COACHES</a></li>
+            <li><a href="leaders.php">LEADERS</a></li>
+            <li><a href="gallery.php">GALLERY</a></li>
+            <li><a href="gallery.php">LATEST NEWS</a></li>
+          </ul>
+          <ul class="mobile">
+            <li><a href="faqs.php">FAQs</a></li>
+            <li><a href="locations.php">LOCATIONS</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+          </ul>
+        </div>
+      </div> -->
+    </nav>
+  </header>
+</div>
