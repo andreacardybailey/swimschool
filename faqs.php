ba2dd5bae1a9ed8db29b2f80ab285a8779c69b25
Andrea Cardy <acardy@customink.com>
2014-11-18 20:18:52 -0500

    Replacement of main homepage image with photoshopped image removing water splash from nose. Addition of FAQ page content.

--- faqs.php
+++ faqs.php
@@ -3,5 +3,36 @@ $pageTitle = 'Frequently Asked Questions';
 include 'modules/main/head.php';
 include 'modules/main/header.php';
 ?>
+<section class="main">
+  <div class="ss-wrapper">
+    <section class="faqs">
+      <h1>Frequently Asked Questions</h1>
+      <h2>What is the best age to start my child in swim lessons?</h2>
+      <p>We begin swim classes at age 3 months.  Submersions do not begin until at least 5 months and this varies based on the children’s comfort level in the water.  It’s never too late to learn.
+      We encourage continuing swim classes without interruption.  Any time away from organized swim classes with a professional teacher will result in regression of skills learned.
+      Learning to swim is a necessity, not a luxury!</p>
 
+      <h2>How long will it take for my child to swim?</h2>
+      <p>Florida Swim School teaches children based on their individual abilities and comfort in the water.  As children grow physiologically, so do their swimming skills.  This allows for children to retain what they have learned in the water and to continue improving their swimming capabilities.</p>
+      <p>Florida Swim School does not suggest a time when swim classes should stop. Keep in mind that it takes 6 months for your a child to learn to crawl and at least 1 year for a child to learn to walk, all this while practicing every day of his/her life!  Be patient, learning to swim is a special skill that requires commitment.  Swimming is a life-saving skill and a skill for life!</p>
+
+      <h2>What if my child cries during the entire lesson?</h2>
+
+      <p>Your child may cry during the lesson and there could be many reasons.  Rest assured that your child is being nurtured during the swim class and that your child is not being harmed or frightened by the teacher.  It is important to remember that everyone involved wants the same result:  safer children in the water.</p>
+
+      <p>Even if your child is crying during the lesson, being in the water is still beneficial since there is learning taking place simply by being in the environment.</p>
+
+      <p>There could be a separation anxiety, especially in ages 2-3.  As a parent, if you expect separation anxiety, we suggest that you join us in the class!  This will show your child that it is safe and that you trust us.  This will also provide for a better understanding of the learn-to-swim process as you create a new bond with your child in the water!</p>
+
+      <p>However, if a parent or caretaker is unable to join the class, we suggest that you remain out of the child’s direct view so that the teacher can begin to build the trust necessary with your child to ensure a positive and successful experience as your child becomes safer in the water.</p>
+
+      <p>It should also be noted that often times your child may not cry at all during the first class, but may cry during the second and third class.  This is due to the first class being new and once it is over, child is back with mommy or daddy.  The second class, your child sees that he is going back to the stranger again.  This explains the importance of continuing with the classes on a regular basis so that teacher and child are able to build a trust between themselves, resulting in a positive experience as your child becomes safer in the water.</p>
+
+      <h2>Why isn’t my child progressing to the next skill like the other students in the class?</h2>
+      <p>All children learn at their own pace.  While we group children according to age, they will still progress individually.  The small ratio of our classes allows for the teachers to practice with each child based on their individual ability.</p>
+
+      <p>There is a natural plateau of skills learned and it is important that the teachers keep children where they are comfortable so that those skills are retained.  If advanced skills are introduced before current skills are learned and practiced, there could be a set-back or, at least, a loss of self-confidence.  Self-confidence is imperative for success in all avenues of life.</p>
+    </section>
+  </div>
+</section>
 <?php include 'modules/main/footer.php'; ?>
