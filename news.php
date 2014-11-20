9535c8c51fadcd6c6614d15255ce13819bd49a61
Andrea Cardy <acardy@customink.com>
2014-11-19 21:35:55 -0500

    Added sidebars

--- news.php
+++ news.php
@@ -7,27 +7,22 @@ include 'modules/main/header.php';
 
 <section class="main">
   <div class="ss-wrapper">
-    <section class="leaders">
+    <section class="news">
       <h1 class="ss-mainhead">November Swimming News</h1>
       <h2 class="ss-subhead">Hello Swimming Families!</h2>
-
       <p>November will be here soon and we want to remind everyone that it will not be too cold to swim!  The pools are heated (small pool 90 degrees, big pool 82 degrees).  It is important for the children to keep practicing so they don’t lose the skills they have learned and also so that they can keep progressing in their swimming abilities.</p>
-
       <p>Swimming will only be cancelled if the air is below 65, which does not happen often.  Those classes will receive make-ups.</p>
-
       <p>Our indoor pool may be ready as soon as November!  Please like Florida Swim School on Facebook and see updates.  Please send any swimming pictures or videos you will allow us to share and we will can post there.</p>
-
       <p><strong>Florida Swim School has an exciting new program for care-takers&hellip;</strong><br />
       Does your child’s care-taker know how to swim?  We are offering 50% discount for the first month of group swimming lessons for these adults.  Parents, this includes you!  Of course, private lessons are also available without a discount.</p>
-
       <p>The adult Stingray group is becoming popular!  They swim Monday and Friday 8:15-9:15 and have fun working out in the pool!  This is a fitness group for those already swimming at least 25 yards.
       Florida Swim School now has <a href="https://floridaswimschoolregistration.com:8102/Loginuser.aspx" target="_blank">online registration</a>! The referral program is always in place.  Refer a friend and receive $20 credit when your friend registers!</p>
-
       <p>There will be no classes Thursday, November 27 for Thanksgiving.  We will have classes on Saturday of that weekend.</p>
-
       <p>Our philosophy is that since children are continually developing physiologically, so are their abilities in the water.  Therefore children should be enrolled in swimming classes regularly so their skills grow with them.  We offer every level of swimming for all abilities and our goal is that the students remain in swimming as their skills become stronger and they become safer.
       Keeping children safer in the water is our priority!</p>
-
+    </section>
+    <section class="ss-sidebar">
+      <?php include 'modules/link_gallery.php' ?>
     </section>
   </div>
 </section>
