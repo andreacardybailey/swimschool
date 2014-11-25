44ee34a7b082501e3e2e5661faac83f763e109cc
Andrea Cardy <acardy@customink.com>
2014-11-25 12:38:36 -0500

    Grammatical corrections.

--- lessons.php
+++ lessons.php
@@ -14,15 +14,15 @@ include 'modules/main/header.php';
       </div>
       <div class="lessons-description">
         <h2>Minnows (6-24 months)</h2>
-        <p>Baby and me group classes for 30 minutes include an adult in the water with the  baby.  Proper handling of baby in the water will be taught, along with promoting comfort in water for baby and adult!  Safety skills will be introduced and practiced in each class.  Private instruction is also available for 15 minutes.</p>
+        <p>Baby & Me group classes for 30 minutes include an adult in the water with the  baby.  Proper handling of baby in the water will be taught, along with promoting comfort in water for baby and adult!  Safety skills will be introduced and practiced in each class.  Private instruction is also available for 15 minutes.</p>
       </div>
       <div class="lessons-description">
         <h2>Turtles (2-3)</h2>
-        <p>Students will be taught in a private setting.  Private classes are 15 minutes and parents are invited, but not required, to be in the water too!  Safety skills taught include going under water and propelling forward, jumping off the side and swimming back to the wall, paddling face down and rolling over to a back float. Kicking on the back is introduced. Skill progression varies as students develop physiologically.</p>
+        <p>Students will be taught in a private setting.  Private classes are 15 minutes and parents are invited, but not required, to be in the water too!  Safety skills taught include:  going under water and propelling forward, jumping off the side and swimming back to the wall, paddling face down and rolling over to a back float. Kicking on the back is introduced. Skill progression varies as students develop physiologically.</p>
       </div>
       <div class="lessons-description">
         <h2>Seahorses (4-5 years)</h2>
-        <p>Classes are 30 minutes with no more than 4 swimmers.  Students in this class will learn all proper swimming skills to provide for a safer child.  Swimmers learn self-propulsion, independent swimming.  Once rolling over is learned, side-breathing and backstroke is taught.  Butterfly and breaststroke drills are introduced.</p>
+        <p>Classes are 30 minutes with no more than 4 swimmers.  Students in this class will learn all proper swimming skills to provide for a safer child.  Swimmers learn self-propulsion and independent swimming.  Once rolling over is learned, side-breathing and backstroke is taught.  Butterfly and breaststroke drills are introduced.</p>
       </div>
       <div class="lessons-description">
         <h2>Dolphins (5-9 years)</h2>
@@ -30,7 +30,7 @@ include 'modules/main/header.php';
       </div>
       <div class="lessons-description">
         <h2>Sharks (Pre-Team)</h2>
-        <p>Students in the Sharks classes are able to swim at least 20 yards of freestyle and backstroke.  Butterfly, breaststroke, and flipturns are taught,  as well as competitive terms and rules.  Pre-Team is 45 minutes in length with no more than 8 students for each teacher.  The focus of this group is endurance, competitive swimming strokes and rules.</p>
+        <p>Students in the Sharks classes are able to swim at least 20 yards of freestyle and backstroke.  Butterfly, breaststroke, and flip turns are taught, as well as competitive terms and rules.  Pre-Team is 45 minutes in length with no more than 8 students for each teacher.  The focus of this group is endurance, competitive swimming strokes and rules.</p>
       </div>
       <div class="lessons-description">
         <h2>Stingrays and Adult Stingrays</h2>
@@ -38,7 +38,7 @@ include 'modules/main/header.php';
       </div>
       <div class="lessons-description">
         <h2>Private Classes or Stroke Mechanics</h2>
-        <p>Classes for students age 4 and over are 20-30 minutes in length.  Proper swimming technique and water safety skills will be taught.  Stroke mechanics is provided for competitive and/or masters swimmers.</p>
+        <p>Classes for students age 4 and over are 20-30 minutes in length.  Proper swimming technique and water safety skills will be taught.  Stroke mechanics are provided for competitive and/or masters swimmers.</p>
       </div>
       <div class="lessons-description lessons-description--lifetime">
         <h2>Lifetime Memberships Available!</h2>
