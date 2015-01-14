1c1f108b1385d43db1706259fcf968676f33d86f
Andrea Cardy <acardy@customink.com>
2015-01-13 20:40:01 -0500

    this is my message

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'modules/main/header.php';
 <section class="home-learnToSwim">
   <div class="ss-wrapper">
     <h1>Learning to swim is a necessity, not a luxury!</h1>
-    <p class="home-blurb">Florida Swim School in Boca Raton, Florida offers swim lessons for all ages and abilities, beginning at 3 months.  Our goal is that the students keep swimming as their skills become stronger to ensure their safety and to develop a love for water. This is my change.</p>
+    <p class="home-blurb">Florida Swim School in Boca Raton, Florida offers swim lessons for all ages and abilities, beginning at 3 months.  Our goal is that the students keep swimming as their skills become stronger to ensure their safety and to develop a love for water. This is my changes.</p>
     <?php include 'modules/link_gallery.php' ?>
 </section>
 
