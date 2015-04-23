be05ee48496e50653de380168bc8129f5a2481fb
Andrea Cardy <acardy@customink.com>
2015-04-23 14:38:57 -0400

    Grammar correction.

--- news.php
+++ news.php
@@ -12,8 +12,8 @@ include 'modules/main/header.php';
       <p>April 1st in the swimming world is known as April Pools Day – to encourage all to become safer in the water.  You are never too old to learn to swim!  April is also a reminder that summer is around the corner and closing in fast!  Schedule swimming now before classes fill up and the sooner you start, the sooner you can have fun at the pool parties!</p>
       <p>Another reminder of summer is that the unique Aqua-Kids Summer Camp in Boca Raton is open for <a href="https://floridaswimschoolregistration.com:8102/Loginuser.aspx" target="_blank">registration online</a>. We will be indoors so weather will not get in the way of our fun! There is a 10% discount for siblings and additional weeks.</p>
       <p>If you are hosting a pool party, please contact Florida Swim School in Boca Raton for a certified lifeguard.  </p>
-      <p>April Showers sometimes get in the way of swim classes at our outdoor facility at Saint Andrew&rsquo;s School.  If the weather prohibits us from using the pool, we will call the families whose classes are affected.  However, we will not cancel in the morning for afternoon lessons, since the weather changes so quickly.  If you prefer not to wait on weather, cancellations must be made by 8am in order to schedule a make-up.</p>
-      <p>Thank you to those who have referred a friend and you have received a $20 credit for each friend who registers!</p>
+      <p>April showers sometimes get in the way of swim classes at our outdoor facility at Saint Andrew&rsquo;s School.  If the weather prohibits us from using the pool, we will call the families whose classes are affected.  However, we will not cancel in the morning for afternoon lessons, since the weather changes so quickly.  If you prefer not to wait on weather, cancellations must be made by 8am in order to schedule a make-up.</p>
+      <p>Thank you to those who have referred a friend. You will receive a $20 credit for each friend who registers!</p>
       <p>See you at the pool!</p>
     </section>
     <section class="ss-sidebar">
