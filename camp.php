66e7909ebff4cdea59f9e7c20cef00328c2ee4a2
Andrea Cardy <acardy@customink.com>
2015-06-18 17:01:06 -0400

    Updated header alert message style. Added new camp dates to homepage and camp page. Updated layout of camp page to be 3 columns.

--- camp.php
+++ camp.php
@@ -14,32 +14,48 @@ include 'modules/main/header.php';
       <p>The forever fun and unique <em>Aqua-Kids Summer Camp</em> returns, this
       time indoors! <a href="https://floridaswimschoolregistration.com:8102/Loginuser.aspx?lo=1">Register online now</a> or by email or mail using the attached <a href="/assets/pdf/Camp_Reg_2015.pdf">registration form</a>.
       </p>
-      <h2>Typical Day: 9:00am-3:00pm</h2>
-        9:00-9:15 Drop-Off/Sign-In<br>
-        9:15-9:45 warm-up/stretching<br>
-        9:45-10:30 Instructional Swim<br>
-        10:30-11:00 Snack<br>
-        11:00-11:45 Arts &amp; Crafts<br>
-        11:45-12:30 Water Polo<br>
-        12:30-1:15 Lunch<br>
-        1:15-2:00 Water Safety/Video/games<br>
-        2:00-2:30 Instructional Swim<br>
-        2:30-2:45 Free Swim<br>
-        3:00 Pick-Up/Sign Out<br>
-        early drop-off/late pick-up available</p>
-        <p>Ages 4-12<br>
-        Bring your lunch!<br>
-        Snacks and water will be provided.<br>
-        $375/week Monday – Friday<br>
-        Half Days available $275/week</p>
-        <p>June 8-12<br>
-        June 15-19<br>
-        July 6-10<br>
-        July 13-17<br>
-        August 3-7<br>
-        August 10-14<br>
-        August 17-21</p>
-
+      <div class="ss-camp-main">
+        <div class="ss-camp-day">
+          <h2>Typical Day: 9:00am-3:00pm</h2>
+            9:00 - 9:15: Drop-Off/Sign-In<br>
+            9:15 - 9:45: Warm-up/Stretching<br>
+            9:45 - 10:30: Instructional Swim<br>
+            10:30 - 11:00: Snack<br>
+            11:00 - 11:45: Arts &amp; Crafts<br>
+            11:45 - 12:30: Water Polo<br>
+            12:30 - 1:15: Lunch<br>
+            1:15 - 2:00: Water Safety/Video/Games<br>
+            2:00 - 2:30: Instructional Swim<br>
+            2:30 - 2:45: Free Swim<br>
+            3:00: Pick-Up/Sign Out</p>
+            <p>**Early drop-off/late pick-up available</p>
+        </div>
+        <div class="ss-camp-details">
+          <h2>Details:</h2>
+          <p>Ages 4-12<br>
+          Bring your lunch!<br>
+          Snacks and water will be provided.</p>
+          <h2>Cost:</h2>
+          <p>
+          $375/week Monday – Friday<br>
+          Half days available: $275/week</p>
+          <p>** 10% discount for siblings and additional weeks.</p>
+        </div>
+        <div class="ss-camp-dates">
+          <h2>Dates:</h2>
+          <p>June 8-12<br>
+          June 15 - 19<br>
+          June 22 - 26<br>
+          June 19 - July 3<br>
+          July 6 - 10<br>
+          July 13 - 17<br>
+          July 20 - 24<br>
+          July 27 - 31<br>
+          August 3 - 7<br>
+          August 10 - 14<br>
+          August 17 - 21</p>
+        </div>
+      </div>
     </section>
   </div>
 </section>
