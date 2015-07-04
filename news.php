c9fc1894e454b244cfdd3dc1073ea8498fc389fe
Andrea Cardy <acardy@customink.com>
2015-07-04 12:19:19 -0400

    July news

--- news.php
+++ news.php
@@ -8,13 +8,21 @@ include 'modules/main/header.php';
 <section class="main">
   <div class="ss-wrapper">
     <section class="news">
-      <h1 class="ss-mainhead">June Swimming News</h1>
-      <p>Summer is in full swing at Florida Swim School! Our beautiful indoor facility is open and the pool is busy! Aqua-Kids Summer Camp includes lots of instruction in the pool, plus pool games, arts and crafts, and movie time. No sunscreen is needed! We will continue to host Aqua-Kids Summer Camp all summer, as demand requires it.</p>
-      <p>Weather will no longer interfere and cancel our swim classes! </p>
-      <p>Baby &amp; Me Classes are quite popular and we are proud to have adults also taking classes. It’s never too late to learn to swim! </p>
-      <p>As you know, summer is the time for pool parties! Have your pool party indoors at Florida Swim School! No need to schedule a &quot;Rain Date&quot; anymore! </p>
-      <p>As always, refer a friend and receive a $25 credit! </p>
-      <p>We look forward to seeing you at our new location&hellip; keep swimming!</p>
+      <h1 class="ss-mainhead">July Swimming News</h1>
+      <p>Swim Classes in Boca Raton for July are already beginning, which means summer is moving quickly! Hopefully you have scheduled swim classes so that you are prepared for all the pool parties!</p>
+
+<p>Speaking of pool parties, be sure to schedule yours indoors with Florida Swim School in Boca Raton!</p>
+
+<p>Now that Florida Swim School’s indoor location is open, we have scheduled our Grand Opening for Saturday, July 18 and open house will be 11am-2pm. We will have pool games, free swim classes, and more!</p>
+
+<p>At the Grand Opening, anyone who registers that day, July 18, will receive 50% off of the registration fee. In addition, 100% of the registration fees collected on July 18 will be donated directly to a representative of the Red Cross, who will be present at the Grand Opening of Florida Swim School in Boca Raton!</p>
+
+<p>Bring your friends!</p>
+
+<p>We hope to see you there!</p>
+
+<p>Keep Swimming and Be Safe!</p>
+
     </section>
     <section class="ss-sidebar">
       <?php include 'modules/openhouse.php' ?>
